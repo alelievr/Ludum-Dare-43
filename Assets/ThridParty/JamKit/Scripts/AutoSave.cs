8405aa3e584b7303c3a62a440fbe04bd7b3d3988
alelievr <alelievr@student.42.fr>
2018-02-24 12:02:42 +0100

    Updatue to 2017.3

---
+++ Assets/ThridParty/JamKit/Scripts/AutoSave.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoSave
+{
+	static public float		saveTimeout = 60 * 2; //2 mins
+
+	static double			lastSave;
+
+	static AutoSave()
+	{
+		EditorApplication.update += Update;
+	}
+
+	static void Update()
+	{
+		if (EditorApplication.timeSinceStartup - lastSave > saveTimeout)
+			Save();
+	}
+
+	static void Save()
+	{
+		if (EditorApplication.isPlaying || EditorApplication.isPaused)
+			return ;
+		EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
+		AssetDatabase.SaveAssets();
+		lastSave = EditorApplication.timeSinceStartup;
+	}
+}
