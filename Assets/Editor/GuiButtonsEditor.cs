729c26c748a50a9cfd5228d0e2239bbf8a7421c4
bciss <cissback@hotmail.fr>
2018-12-01 23:46:54 +0100

    base guiman active controlle

---
+++ Assets/Editor/GuiButtonsEditor.cs
@@ -0,0 +1,46 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEditor;
+// using UnityEditorInternal;
+
+// [CustomEditor(typeof(GuiButtons))]
+// public class GuiButtonsEditor : Editor
+// {
+//     GuiButtons      buttons;
+//     ReorderableList list;
+
+//     private void OnEnable()
+//     {
+//         buttons = target as GuiButtons;
+//         list = new ReorderableList(buttons.buttonList, typeof(GuiButtons.ButtonList));
+
+//         list.onAddCallback = (list) => {
+//             buttons.buttonList.Add(new GuiButtons.ButtonList{
+//                 index = 0,
+//                 actif = true,
+//             });
+//         };
+
+//         list.drawElementCallback = (rect, index, active, selected) => {
+//             rect.height = EditorGUIUtility.singleLineHeight;
+//             Rect labelRect = rect;
+//             Rect gameObjectRect = rect;
+//             gameObjectRect.y += EditorGUIUtility.singleLineHeight + 6;
+//             Rect actifRect = rect;
+//             actifRect.y += EditorGUIUtility.singleLineHeight * 2 + 8;
+//             // buttons.buttonList[index].name = EditorGUI.TextField(labelRect, "Name", buttons.buttonList[index].name);
+//             buttons.buttonList[index].index = (PlayerController.Key)EditorGUI.Popup(labelRect, "Key", (int)buttons.buttonList[index].index, System.Enum.GetNames(typeof(PlayerController.Key)));
+//             buttons.buttonList[index].guiButton = EditorGUI.ObjectField(gameObjectRect, "GUI button", buttons.buttonList[index].guiButton as Object, typeof(GameObject), true) as GameObject;
+//             buttons.buttonList[index].guiLayer = EditorGUI.ObjectField(gameObjectRect, "GUI Layer", buttons.buttonList[index].guiLayer as Object, typeof(GameObject), true) as GameObject;
+//             buttons.buttonList[index].actif = EditorGUI.Toggle(actifRect, "Active", buttons.buttonList[index].actif);
+//         };
+
+//         list.elementHeight = EditorGUIUtility.singleLineHeight * 4 + 16;
+//     }
+
+//     public override void OnInspectorGUI()
+//     {
+//         list.DoLayoutList();
+//     }
+// }
---
+++ Assets/Scripts/GuiButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class GuiButtons : MonoBehaviour
+{
+    [System.Serializable]
+    public class ButtonList {
+            public PlayerController.Key index;
+            public GameObject guiButton;
+            public GameObject guiLayer;
+            public bool actif;
+    }
+    public List<ButtonList> buttonList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetButtonStatus(PlayerController.Key p_button, bool p_status) {
+        ButtonList button = buttonList.Where(c => c.index == p_button).FirstOrDefault();
+        button.actif = p_status;
+        button.guiLayer.SetActive(!p_status);
+    }
+
+    public bool GetButtonStatus(PlayerController.Key p_button) {
+        return buttonList.Where(c => c.index == p_button).FirstOrDefault().actif;
+    }
+}
