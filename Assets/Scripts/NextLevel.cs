7f450503a3aeea70653f3f7cb48ceff29377a87c
Gabriel FOURNIER <gabfou95@gmail.com>
2018-12-03 21:11:53 +0100

    dsa

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -3,26 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NextLevel : MonoBehaviour {
+public class NextLevel : SceneTransition {
 
 	public string nexTSceneName;
-	public float WaitInSeconds = 2;
 	public string tagAccepted = "Player";
 	// Use this for initialization
-	
-	IEnumerator WaitForNext()
-	{
-		yield return new WaitForSeconds(WaitInSeconds);
-		SceneManager.LoadScene(nexTSceneName);
-	}
+
 
 	void OnTriggerStay2D(Collider2D other)
 	{
-		Debug.Log("sdfads");
 		if (other.tag == tagAccepted)
 		{
-			StartCoroutine(WaitForNext());
-			Debug.Log("sdfad2222s");
+			base.LoadScene(nexTSceneName);
 		}
 
 	}
