03b74a2ff99cde51976a69ef199e4c8719cc423b
alelievr <alelievr@student.42.fr>
2018-12-03 23:25:13 +0100

    Added scripted attack

---
+++ Assets/TriggerTimeline.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerTimeline : MonoBehaviour
+{
+    public PlayableDirector director;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        director.Play();
+    }
+}
