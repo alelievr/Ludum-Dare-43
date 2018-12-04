918910590931656cdbf3ea2e404cb7684bef45c6
Jean ROMAGNANI <jromagna@student.42.fr>
2018-12-04 01:14:16 +0100

    LINTRO

---
+++ Assets/Scripts/IntroEnder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class IntroEnder : SceneTransition
+{
+    private void OnEnable()
+    {
+        base.LoadScene("level2");
+    }
+}
